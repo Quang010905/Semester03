49251a8bcb499a866e49054e4992d36e49631baf
TrTamDev <nguyentructam2495@gmail.com>
2025-11-08 13:34:04 +0700

    demo1 admin view

--- Areas/Admin/Controllers/AdminController.cs
+++ Areas/Admin/Controllers/AdminController.cs
@@ -1,13 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Semester03.Areas.Admin.Models;
+using Semester03.Models.Entities;
 
 namespace Semester03.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    // [Authorize(Roles = "Super Admin, Mall Manager")] // Sẽ thêm khi làm Login
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly AbcdmallContext _context;
+        public AdminController(AbcdmallContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new AdminDashboardViewModel
+            {
+                TotalUsers = await _context.TblUsers.CountAsync(),
+                TotalTenants = await _context.TblTenants.CountAsync(),
+                TotalMovies = await _context.TblMovies.CountAsync(),
+                TotalTicketsSold = await _context.TblTickets.CountAsync()
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ Areas/Admin/Models/AdminDashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Semester03.Areas.Admin.Models
+{
+    public class AdminDashboardViewModel
+    {
+        public int TotalUsers { get; set; }
+        public int TotalTenants { get; set; } // Tổng số cửa hàng
+        public int TotalMovies { get; set; }  // Tổng số phim
+        public int TotalTicketsSold { get; set; } // Tổng số vé
+    }
+}
