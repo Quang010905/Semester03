44cd3c3c0d3ed11121f3553ee54dfe768c36126e
0868397300 <trongnguyenphu2509@gmail.com>
2025-11-11 16:49:44 +0700

    Details cinema not complete

---
+++ Areas/Client/Models/ViewModels/MovieDetailsVm.cs
@@ -0,0 +1,29 @@
+﻿namespace Semester03.Areas.Client.Models.ViewModels
+{
+    public class MovieDetailsVm
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string PosterUrl { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? Rate { get; set; }
+        public int? DurationMin { get; set; }
+        public string Description { get; set; }
+
+        public List<CommentVm> Comments { get; set; } = new List<CommentVm>();
+    }
+
+    public class CommentVm
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public string UserName { get; set; }
+        public int Rate { get; set; }
+        public string Text { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+
+}
